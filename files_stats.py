2411740e6d5b2269abf81c629a4d1c7bc2526674
Kathrin Heim <kathrin.heim@gmail.com>
2025-11-26 07:51:57 +0100

    Update files_stats.py

--- files_stats.py
+++ files_stats.py
@@ -9,45 +9,58 @@ load_dotenv()
 ACCESS_TOKEN = os.getenv("ZENODO_TOKEN")
 
 ZENODO_API = "https://zenodo.org/api/records"
-COMMUNITY = "lory_phlu"  # Name der Community
+# Name der Community festlegen
+COMMUNITY = "lory_hslu"  
+page = 1
+total_bytes = 0
 
 
-def get_records(page=1):
-    params = {
+headers = {}
+headers["Content-Type"] = "application/json"
+headers["Authorization"] = f"Bearer {ACCESS_TOKEN}"
+
+params = {
         "communities": COMMUNITY,
-        "page": page,
-        "size": 100,
+        "size": 25,
     }
 
-    # Token nur senden, wenn vorhanden
-    if ACCESS_TOKEN:
-        params["access_token"] = ACCESS_TOKEN
 
-    return requests.get(ZENODO_API, params=params).json()
+def getRecords():
+    r = requests.get(f"{ZENODO_API}", params=params, headers=headers)
+    print("Status:", r.status_code)
+    print("Headers:", r.headers)
+    print("Text:", r.text[:500])
+    return r.json()
 
+result = getRecords()
+#print(result["status"])
+numOfRec = (result["hits"]["total"])
+print("Records: ", numOfRec)
 
-def sum_total_bytes():
-    total_bytes = 0
-    page = 1
+localRecordCounter = 1
+remotePaginator = 1
+resultSet = {}
+while localRecordCounter < int(numOfRec):
+    params["page"] = remotePaginator
+    result = getRecords()
+    for record in result["hits"]["hits"]:
+        print(f"#{localRecordCounter}: {record['title']}")
+        localRecordCounter += 1
+        files = record.get("files", [])
+        for f in files:
+            print(f"{f['size']/1e6:.3f} MB") 
 
-    while True:
-        data = get_records(page)
-        hits = data.get("hits", {}).get("hits", [])
-
-        if not hits:
-            break
-
-        for record in hits:
-            files = record.get("files", [])
-            for f in files:
-                size = f.get("size", 0)
-                total_bytes += size
+            size = f.get("size", 0)
+            total_bytes += size
+            
+                
 
         page += 1
+    remotePaginator += 1
+    print(f"go to next records page {remotePaginator}")
 
-    return total_bytes
 
 
-if __name__ == "__main__":
-    total = sum_total_bytes()
-    print(f"Total size of all files in community '{COMMUNITY}': {total / 1e9:.3f} GB")
+
+print(f"Total size of {numOfRec} files in community '{COMMUNITY}': {total_bytes / 1e9:.3f} GB")
+print(f"or  {total_bytes / 1e6:.3f} MB")
